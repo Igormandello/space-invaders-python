795c28546a0c2782751b0fdca6a5145ed51cf69f
Igormandello <igormandello@gmail.com>
2018-11-07 11:48:05 -0200

    Adds a initial screen image and animation

--- src/controller.py
+++ src/controller.py
@@ -23,7 +23,7 @@ class Controller:
     self.screen = pg.display.set_mode(size)
     self.done = False
 
-    self.scene_controller = SceneController([(0, 0, 0), (40, 40, 40), (80, 80, 80), (120, 120, 120)], self.reset, self.screen)
+    self.scene_controller = SceneController([['./assets/initialScreen1.png', './assets/initialScreen2.png'], (0, 0, 0), (80, 80, 80), (120, 120, 120)], self.reset, self.screen)
     self.player = Player((size[0] / 2 - PLAYER_SIZE / 2, size[1] - PLAYER_SIZE * 5 / 4), (PLAYER_SIZE, PLAYER_SIZE), 3, './assets/player.png', self.screen)
     self.shot_controller = ShotController(size[1] - PLAYER_SIZE * 5 / 4, (SHOT_WIDTH, SHOT_HEIGHT), 8, './assets/shot.png', self.screen, 60)
     self.invaders_controller = InvadersController(4, 7, 10, (30, 20), (34, 25), ['./assets/invader.png', './assets/invader2.png'], self.screen)
--- src/scene_controller.py
+++ src/scene_controller.py
@@ -7,13 +7,23 @@ class SceneController:
       if type(backgrounds[i]) == tuple:
         self.backgrounds.append(lambda: display.fill(backgrounds[i]))
       elif type(backgrounds[i]) == str:
-        image = pg.image.load(b).convert_alpha()
+        image = pg.image.load(backgrounds[i]).convert_alpha()
         image = pg.transform.scale(image, (display.get_width(), display.get_height()))
         self.backgrounds[i] = image
+      elif type(backgrounds[i]) == list:
+        images = []
+        for img in backgrounds[i]:
+          image = pg.image.load(img).convert_alpha()
+          image = pg.transform.scale(image, (display.get_width(), display.get_height()))
+          images.append(image)
+
+        self.backgrounds[i] = images
 
     self.reset = reset_fn
     self.display = display
     self.current = 0
+    self.frame = 0
+    self.index = 0
 
   def in_game(self):
     return self.current == 1
@@ -21,18 +31,31 @@ class SceneController:
   def process_action(self): 
     if self.current > 1:
       self.current = 1
+      self.frame = 0
       self.reset()
     elif self.current == 0:
       self.current = 1
+      self.frame = 0
 
   def win(self):
     self.current = 2
+    self.frame = 0
 
   def lose(self):
     self.current = 3
+    self.frame = 0
 
   def update(self):
+    self.frame += 1
     if type(self.backgrounds[self.current]) == tuple:
       self.display.fill(self.backgrounds[self.current])
-    else:
+    elif type(self.backgrounds[self.current]) == pg.Surface:
       self.display.blit(self.backgrounds[self.current], (0, 0))
+    elif type(self.backgrounds[self.current]) == list:
+      if self.frame % 100 == 0:
+        self.frame = 0
+        self.index += 1
+        if self.index > 1:
+          self.index = 0
+
+      self.display.blit(self.backgrounds[self.current][self.index], (0, 0))
